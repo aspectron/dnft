0bcf988af82b0a6b18be6fa94dfda5c6d18f97e1
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2023-03-03 15:33:05 -0500

    sale type

--- lib/src/program/exchange.rs
+++ lib/src/program/exchange.rs
@@ -1,6 +1,17 @@
 use crate::prelude::*;
 
 #[derive(Clone, BorshSerialize, BorshDeserialize)]
+#[repr(u16)]
+pub enum SaleType {
+    NoSale = 0,
+    Sale = 0x0101,
+    Rent = 0x0102,
+    Auction = 0x0103,
+    Barter = 0x0104,
+    Raffle = 0x0105,
+}
+
+#[derive(Clone, BorshSerialize, BorshDeserialize)]
 pub enum Sale {
     Sol { price: u64 },
     Spl { token: Pubkey, price: u64 },
@@ -21,10 +32,10 @@ pub struct Rent {}
 #[derive(Clone, BorshSerialize, BorshDeserialize)]
 pub enum ExchangeMechanics {
     Sale(Sale),
-    Auction(Auction),
-    Raffle(Raffle),
-    Barter(Barter),
     Rent(Rent),
+    Auction(Auction),
+    Barter(Barter),
+    Raffle(Raffle),
 }
 
 // #[derive(Clone, BorshSerialize, BorshDeserialize)]
